1809cc4fe59d2a51e92351e3a5745889ec24205a
Aisling Fontaine <aisling.fontaine@protonmail.com>
2024-11-20 18:02:16 +0100

    add argv support and some comments

--- webclient.py
+++ webclient.py
@@ -1,16 +1,25 @@
 import socket
+import sys
 
-target = 'renshuu.org'
+target = sys.argv[1]
+request_text = f"GET / HTTP/1.1\r\nHost: {target}\r\nConnection: close\r\n\r\n"
+request_bytes = request_text.encode('ISO-8859-1')
 
 s = socket.socket()
 s.connect((target, 80))
-s.sendall(b'GET / HTTP/1.1\r\nHost: google.com\r\nConnection: close\r\n\r\n')
+s.sendall(request_bytes)
+
+""" Get response in chunks of 1024 bytes, d is the buffer and it all gets sent
+    eventually to answer.
+"""
 d = s.recv(1024)
 answer = d
 while len(d) != 0:
     d = s.recv(1024)
     answer = answer.__add__(d)
 
+""" Wrap it up and print out the answer gotten from the server.
+"""
 s.close()
 answer = answer.decode('ISO-8859-1')
 print(answer)
