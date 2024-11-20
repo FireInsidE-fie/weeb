4a76112b42cbc818127e845563338434ac2f6123
Aisling Fontaine <aisling.fontaine@protonmail.com>
2024-11-20 16:23:43 +0100

    add stup for client

--- webclient.py
+++ webclient.py
@@ -1,2 +1,12 @@
 import socket
 
+s = socket.socket()
+s.connect(('google.com', 80))
+s.sendall(b'GET / HTTP/1.1\r\nHost: google.com\r\nConnection: close\r\n\r\n')
+d = s.recv(1024)
+
+#while len(d) != 0:
+#    d = s.recv(1024)
+
+s.close()
+print(d)
