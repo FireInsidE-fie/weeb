f9f054887a1793aff3d8c92dd958975308a2f141
Aisling Fontaine <aisling.fontaine@protonmail.com>
2024-11-20 17:32:19 +0100

    add loop to get entire answer from server

--- webclient.py
+++ webclient.py
@@ -1,15 +1,16 @@
 import socket
 
-target = 'google.com'
+target = 'renshuu.org'
 
 s = socket.socket()
 s.connect((target, 80))
 s.sendall(b'GET / HTTP/1.1\r\nHost: google.com\r\nConnection: close\r\n\r\n')
 d = s.recv(1024)
-
-#while len(d) != 0:
-#    d = s.recv(1024)
+answer = d
+while len(d) != 0:
+    d = s.recv(1024)
+    answer = answer.__add__(d)
 
 s.close()
-answer = d.decode('ISO-8859-1')
+answer = answer.decode('ISO-8859-1')
 print(answer)
